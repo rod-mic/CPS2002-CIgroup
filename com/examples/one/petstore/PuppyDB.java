5f403cbfb286be8511df89b9055b4e371e3a07d0
tmuscat95 <timothy.muscat.13@um.edu.mt>
2017-02-28 13:21:07 +0100

    PuppyDB

---
+++ com/examples/one/petstore/PuppyDB.java
@@ -0,0 +1,24 @@
+package com.examples.one.petstore;
+
+class PuppyDB{
+
+    ArrayList <Puppy> Puppies;
+
+    boolean addPuppy(Puppy p){
+
+
+    }
+
+    boolean delPuppy(int id){
+
+
+    }
+
+    boolean getPuppy(int id) {
+
+    }
+
+    int countPuppies(){
+
+    }
+}
