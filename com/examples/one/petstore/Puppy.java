3f2715954bfdb0b7948e546239685ffe7211236c
steve-attard <steve.attard.15@um.edu.mt>
2017-02-28 14:26:42 +0200

    finished puppy class

--- com/examples/one/petstore/Puppy.java
+++ com/examples/one/petstore/Puppy.java
@@ -1,5 +1,64 @@
 package com.examples.one.petstore;
 
 class Puppy{
+    int id;
+    String name;
+    String breed;
+    boolean loudBark;
+    int exerciseRequiredPerDay;
+    boolean friendlyWithChildren;
 
+    public Puppy(int id, String name, String breed) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+
+    public boolean getLoudBark() {
+        return this.loudBark;
+    }
+
+    public int getExerciseRequiredPerDay() {
+        return this.exerciseRequiredPerDay;
+    }
+
+    public int getFriendlyWithChildren() {
+        return this.friendlyWithChildren;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setLoudBark(boolean loudBark) {
+        this.loudBark = loudBark;
+    }
+
+    public void setExerciseRequiredPerDay(int erpd) {
+        this.exerciseRequiredPerDay = erpd;
+    }
+
+    public void setFriendlyWithChildren(boolean fwc) {
+        this.friendlyWithChildren = fwc;
+    }
 }
