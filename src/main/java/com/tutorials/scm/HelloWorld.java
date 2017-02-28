aa71b1c78139432b147764d66c00654f6cce1b1f
JARVIS\Thomas <thomas.m.mallia.15@um.edu.mt>
2017-02-28 12:39:16 +0100

    added sample code to repository

---
+++ src/main/java/com/tutorials/scm/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorials.scm;
+
+/**
+ * Created by joseph.masini on 17/02/2017.
+ */
+public class HelloWorld {
+
+    public static void main(final String args[]) {
+
+        System.out.println("Hello World!");
+    }
+
+}
